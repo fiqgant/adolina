2ed3dc4fcfe8ced60e496443e24b71f63fd68b3d
fiqgant <fiqgant@gmail.com>
2022-08-11 09:56:44 +0700

    commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import randint
 for i in range(1,365):
   for j in range(0,randint(1,10)):
     d=str(i)+' days ago'
-  with open ('README.md','a')as file:
+  with open ('file.txt','a')as file:
     file.write(d)
 os.system('git add .')
 os.system('git commit --date="'+d+'" -m "commit"')
